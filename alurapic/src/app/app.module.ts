72d494ededd81f251b758e8c678af1bafdc4c3ec
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-03-09 09:04:19 -0300

    home module

--- alurapic/src/app/app.module.ts
+++ alurapic/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
 
-import { PhotosModule } from './photos/photos.module';
-import { AppRoutingModule } from './app.routing.module';
-import { ErrorsModule } from './errors/errors.module';
+import { PhotosModule } from "./photos/photos.module";
+import { AppRoutingModule } from "./app.routing.module";
+import { ErrorsModule } from "./errors/errors.module";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-    ErrorsModule
+    ErrorsModule,
+    HomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ alurapic/src/app/home/signin.component.ts
@@ -0,0 +1,4 @@
+import { Component } from "@angular/core";
+
+@Component({ templateUrl: "./signin.component.html" })
+export class SignInComponent {}
