d829a5c896a7100d7c16f61e11628e5c511cfe91
GabrielaPiovezan <gabrielapiovezan@live.com>
2021-03-09 10:54:45 -0300

    configured route

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { SignInComponent } from "./signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [CommonModule, ReactiveFormsModule, VMessageModule],
+  imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule],
 })
 export class HomeModule {}
